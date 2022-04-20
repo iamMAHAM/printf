d93357bd1ec2e1fbd497f116c684ccc85402036c
MAHAM <abdulkabore@gmail.com>
2022-04-20 12:12:33 +0200

    update func2.c

--- functions2.c
+++ functions2.c
@@ -21,7 +21,7 @@ int print_S(va_list args)
 			_putchar('\\');
 			_putchar('x');
 			total += 2;
-			total += print_hex(str[i]);
+			total += hex_print(str[i]);
 		}
 		else
 		{
@@ -30,4 +30,28 @@ int print_S(va_list args)
 		}
 	}
 	return (total);
+}
+
+/**
+ * hex_print - prints a char's ascii value in uppercase hex
+ * @c: char to print
+ *
+ * Return: number of chars printed (always 2)
+ */
+int hex_print(char c)
+{
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
 }
