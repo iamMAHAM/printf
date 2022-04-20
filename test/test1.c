9b75e9a05f688573a1c9e55146217418a659b8cb
MAHAM <abdulkabore@gmail.com>
2022-04-20 15:10:02 +0000

    update test to test S handling

--- test/test1.c
+++ test/test1.c
@@ -2,13 +2,26 @@
 #include <stdlib.h>
 #include "../main.h"
 
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	int ab1, ab2;
-
-	ab1 = printf("Salut %c tu as %d et %r percent %% tu t'appelle %s\n et", 'H', 29, "Souleymane");
-	ab2 = _printf("Salut %c tu as %d et %r percent %% tu t'appelle %s\n et %b", 'H', 29, "Souleymane", 98);
-	printf("standard printf : %d\n", ab1);
-	printf("our printf : %d\n", ab2);
-	return (0);
+    _printf("%S\n", "Best\nSchool");
+    int a = _printf("%u", 0);
+    _putchar('\n');
+    int b = _printf("%S", "\n");
+    _putchar('\n');
+    int c = _printf("%S", "\x01\x02\x03\x04\x05\x06\x07");
+    int c1 = printf("%S", "\x01\x02\x03\x04\x05\x06\x07");
+    _putchar('\n');
+    int d = _printf("Could you print some non-prntable characters?\n%S\nThanks!\n", "Sure:\x1F\x7F\n");
+    _putchar('\n');
+    int e = _printf("- What did you say?\n- %S\n- That's what I thought.\n", "");
+    int f = _putchar('\n');
+    printf("a = %d\nb = %d\nc = %d & r = %d\nd = %d\ne = %d\nf = %d\n", a, b, c, c1, d, e, f);
+    return (0);
 }
