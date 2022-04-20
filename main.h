1d5a99ca859fffda612b3fef2bd7cad3880c781c
MAHAM <abdulkabore@gmail.com>
2022-04-20 15:51:18 +0000

    update main.h

--- main.h
+++ main.h
@@ -16,21 +16,36 @@ typedef struct func_spec
 	int (*func_ptr)(va_list);
 } func_spec;
 
+
+/*_printf.c*/
 int _printf(const char *format, ...);
+
+/*_putchar.c*/
 int _putchar(char c);
+
+/*print_dec.c*/
+int print_dec(va_list args);
+
+/*get_functions.c*/
 int (*get_func(const char *format))(va_list);
+
+/*functions.c*/
 int print_char(va_list args);
 int print_str(va_list args);
 int print_pct(va_list args);
-int print_dec(va_list args);
 int print_nothing(const char *format, int i);
 int convert_binary(va_list args);
+
+/*functions1.c*/
 int print_unsigned(va_list args);
 int print_octal(va_list args);
 int print_hex(unsigned int n, unsigned int c);
 int print_lhex(va_list args);
 int print_uhex(va_list args);
+
+/*functions2.c*/
 int print_S(va_list args);
 int hex_print(char c);
+int print_pointer(va_list p);
 
 #endif
