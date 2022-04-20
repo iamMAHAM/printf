6a660ef85e6e2e4f4a2a71422c7c225257c7305d
MAHAM <abdulkabore@gmail.com>
2022-04-20 11:38:52 +0200

    add functions 2 with S handle

---
+++ functions2.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_S - prints a string and nonprintable character ascii values
+ * @args: the list of args
+ * Return: number of chars printed
+ */
+int print_S(va_list args)
+{
+	unsigned int i;
+	int total = 0;
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			total += 2;
+			total += hex_print(str[i]);
+		}
+		else
+		{
+			_putchar(str[i]);
+			total++;
+		}
+	}
+	return (total);
+}
