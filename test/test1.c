94bfbf04bfe4ddde4307d63a38e037f24cf71cd1
MAHAM <abdulkabore@gmail.com>
2022-05-03 10:46:15 +0000

    update test file

--- test/test1.c
+++ test/test1.c
@@ -10,18 +10,6 @@
  */
 int main(void)
 {
-    _printf("%S\n", "Best\nSchool");
-    int a = _printf("%u", 0);
-    _putchar('\n');
-    int b = _printf("%S", "\n");
-    _putchar('\n');
-    int c = _printf("%S", "\x01\x02\x03\x04\x05\x06\x07");
-    int c1 = printf("%S", "\x01\x02\x03\x04\x05\x06\x07");
-    _putchar('\n');
-    int d = _printf("Could you print some non-prntable characters?\n%S\nThanks!\n", "Sure:\x1F\x7F\n");
-    _putchar('\n');
-    int e = _printf("- What did you say?\n- %S\n- That's what I thought.\n", "");
-    int f = _putchar('\n');
-    printf("a = %d\nb = %d\nc = %d & r = %d\nd = %d\ne = %d\nf = %d\n", a, b, c, c1, d, e, f);
-    return (0);
+	_printf("%R", "Salut qui est tu toi ? mdrrrrrr");
+	return (0);
 }
